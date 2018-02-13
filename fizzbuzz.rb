225549690d1e6bffd52af445a27949340f19df22
Darcy McCartney <darcy.mccartney@wework.com>
2018-02-13 00:14:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 #The goal of FizzBuzz is to build a program that can take a number, & if the number is evenly divisible by 3, it should return "Fizz", if it's divisible by 5, it should return "Buzz", and if it's divisible by both 3 and 5, it should return "FizzBuzz".
 
-dictionary [Fizz, Buzz, FizzBuzz, Nil]
+#def fizzbuzz(int)
+#  if int % 5 == 0 && int % 3 == 0
+ #   "FizzBuzz" 
+  #  elsif int % 5 == 0
+   # "Buzz"
+    #elsif int % 3 == 0
+    #"Fizz"
+  #end
+end
 
 def fizzbuzz(int)
   int = dictionary
