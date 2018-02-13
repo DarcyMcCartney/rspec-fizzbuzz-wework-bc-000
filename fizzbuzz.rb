206cbebd43f2ce1cef392dc4d4f224633ca850cb
Darcy McCartney <darcy.mccartney@wework.com>
2018-02-13 00:08:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 #The goal of FizzBuzz is to build a program that can take a number, & if the number is evenly divisible by 3, it should return "Fizz", if it's divisible by 5, it should return "Buzz", and if it's divisible by both 3 and 5, it should return "FizzBuzz".
 
 def fizzbuzz(int)
-if int % 3 == 0 && int % 5 == 0
-  puts "FizzBuzz"
-elsif int % 3 == 0
+if int % 3 == 0 && int % 5 !== 0
   puts "Fizz"
-elsif int % 5 == 0
+  elsif int % 5 == 0 && int % 3 !== 0
   puts "Buzz"
+elsif int % 3 == 0 && int % 5 == 0
+  puts "FizzBuzz"
 else puts "nil"
 end
 end
